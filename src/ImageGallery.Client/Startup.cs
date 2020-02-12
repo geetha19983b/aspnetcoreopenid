54346a9f006f213930c72fb720d2157db19003ad
Geethagajalakshmi B <Geethagajalakshmi_B@ad.infosys.com>
2020-02-12 10:59:51 +0530

    Add project files.

---
+++ src/ImageGallery.Client/Startup.cs
@@ -0,0 +1,125 @@
+﻿using ImageGallery.Client.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IdentityModel;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ImageGallery.Client
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+ 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            //a new policy which allows frame order only if the country is belgium and user is
+            //paying user
+            services.AddAuthorization(authorizationOptions =>
+            {
+                authorizationOptions.AddPolicy(
+                    "CanOrderFrame",
+                    policyBuilder =>
+                    {
+                        policyBuilder.RequireAuthenticatedUser();
+                        policyBuilder.RequireClaim("country", "be");
+                        policyBuilder.RequireClaim("subscriptionlevel", "PayingUser");
+                    });
+            });
+            // register an IHttpContextAccessor so we can access the current
+            // HttpContext in services by injecting it
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // register an IImageGalleryHttpClient
+            services.AddScoped<IImageGalleryHttpClient, ImageGalleryHttpClient>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            }).AddCookie("Cookies",
+              (options) =>
+              {
+                  //access denied page when free user tries to access order page
+                  options.AccessDeniedPath = "/Authorization/AccessDenied";
+              })
+              .AddOpenIdConnect("oidc", options =>
+              {
+                  options.SignInScheme = "Cookies";
+                  //idp url
+                  options.Authority = "https://localhost:44379";
+                  options.ClientId = "imagegalleryclient";
+                  options.ResponseType = "code id_token";
+                  //options.CallbackPath = new PathString("...")
+                  //options.SignedOutCallbackPath = new PathString("...")
+                  options.Scope.Add("openid");
+                  options.Scope.Add("profile");
+                  //get address info
+                  options.Scope.Add("address");
+                  options.Scope.Add("roles");
+                  options.Scope.Add("subscriptionlevel");
+                  options.Scope.Add("country");
+                  //api scope
+                  options.Scope.Add("imagegalleryapi");
+                  //to enable getting refresh token if app is offline
+                  options.Scope.Add("offline_access");
+                 
+                  options.SaveTokens = true;
+                  options.ClientSecret = "secret";
+                  //use the identity token and through back channel invokes user info end point to get the claims
+                  options.GetClaimsFromUserInfoEndpoint = true;
+                  //remove the below info from the jwt
+                  options.ClaimActions.Remove("amr");
+                  options.ClaimActions.DeleteClaim("sid");
+                  options.ClaimActions.DeleteClaim("idp");
+                  //options.ClaimActions.DeleteClaim("address");
+                  //role info  map address mapped by default so no need to add for address
+                  options.ClaimActions.MapUniqueJsonKey("role", "role");
+                  options.ClaimActions.MapUniqueJsonKey("subscriptionlevel", "subscriptionlevel");
+                  options.ClaimActions.MapUniqueJsonKey("country", "country");
+
+                  //add it to validation params so that it will be available to check at controller 
+                  //action attribute level if user is  in particular role
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      NameClaimType = JwtClaimTypes.GivenName,
+                      RoleClaimType = JwtClaimTypes.Role,
+                  };
+              });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
+            }
+            app.UseAuthentication();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Gallery}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
